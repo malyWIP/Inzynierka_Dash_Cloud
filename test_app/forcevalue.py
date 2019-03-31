9833d3f46d114cfd2a70944e9de3a079424dba41
Mateusz <nowak543210@gmail.com>
2019-03-31 11:48:57 +0200

    Dodanie 2 stref z 4 do analizy wykresu

--- test_app/forcevalue.py
+++ test_app/forcevalue.py
@@ -42,12 +42,58 @@ def force_motion_value(plots):
             line_count += 1
             row_count +=1
         elif line_count > 280 and row != 'Sequence Editor':
-                x.append(float(row[1]))
+
                 y.append(float(row[2]))
                 line_count += 1
                 row_count += 1
     return x, y
 
+def Pomiar_sil(plots):
+    max_force = float(data_separate(file_to_analizes())[8])
+    print(max_force)
+    line_count = 0
+    row_count = 0
+    y = []
+    peak = 0
+    for row in plots:
+        if line_count <= 280:
+            line_count += 1
+            row_count += 1
+        elif line_count > 280 and row != 'Sequence Editor' and peak != 1:
+                peak = y.count(max_force)
+                z = round(float(row[2]),2)
+                y.append(z)
+                line_count += 1
+                row_count += 1
+        else:
+            line_count += 1
+    return  y
+
+def Delta_Force(delta_force):
+    strefa_1 = []
+    strefa_2 = []
+    change = 0
+    try:
+        for w in range(len(delta_force)):
+            z = float(delta_force[w+1] - delta_force[w])
+            round(z,2)
+            if z < 50 and change != 1:
+                strefa_1.append(float(round(z,4)))
+            else:
+                change = 1
+                strefa_2.append(float(round(z,4)))
+        return strefa_1,strefa_2
+    except IndexError:
+        print('brak do przeliczenia liczb')
+        return strefa_1,strefa_2
+
+
+def Przeliczena_elo():
+    x  = [1,2,3,4,5,6]
+    y = [1,2,3,4,5,6]
+    for w in range(0,len(x)):
+        z= y[w] - x[w]
+        print(z)
 
 def data_separate(plots):
     line_count = 0
@@ -55,13 +101,35 @@ def data_separate(plots):
     for row in plots:
         if line_count < 10:
             line_count += 1
-        elif line_count <14:
+        elif line_count <13: #Data,czas
             y.append((row[1]))
             line_count += 1
-        elif line_count < 31:
+        elif line_count == 15: #numer pomiaru
+            y.append((row[1]))
             line_count += 1
-        elif line_count < 35:
+        elif line_count == 17: #Nazwa
+            y.append((row[1]))
+            line_count += 1
+        elif line_count < 30:
+            line_count += 1
+        elif line_count < 32: #ostatnia wartosc x,y
             y.append(float(row[1]))
+            line_count += 1
+        elif line_count == 34: #Peak
+            y.append(float(row[1]))
+            line_count += 1
+        elif line_count == 37:  # Peak
+            y.append(float(row[4]))
+            line_count += 1
+        elif line_count < 223:
+            line_count += 1
+        elif line_count < 225: #Granice pomiaru w dlugosci
+            y.append((row[2]))
+            line_count += 1
+        elif line_count < 270:
+            line_count += 1
+        elif line_count == 270:  #Liczba pomiarow
+            y.append((row[1]))
             line_count += 1
         # elif line_count < 34:
         #     line_count += 1
@@ -84,11 +152,12 @@ def motion_value(plots):
             line_count += 1
     return y
 
-# if __name__ == '__main__':
-#     while True:
-#
-#         print(data_separate(file_to_analizes()))
-#         # print(motion_value(file_to_analizes()))
-#         # print(force_value(file_to_analizes()))
-#
-#         time.sleep(10)
+if __name__ == '__main__':
+    while True:
+
+        # print(data_separate(file_to_analizes()))
+        # print(motion_value(file_to_analizes()))
+        # print(force_value(file_to_analizes()))
+        # print(Pomiar_sil(file_to_analizes()))
+        print(Delta_Force(Pomiar_sil(file_to_analizes())))
+        time.sleep(10)
