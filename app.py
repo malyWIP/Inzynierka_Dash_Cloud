3173a19cef2ec7f8fe606acfb4b2915a18efd882
Mateusz <nowak543210@gmail.com>
2019-03-31 23:23:58 +0200

    dzialajace 4 strefy nalezy poddac analizie

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ global process_tester
 process_tester = DataMove(True)
 reset_data = DataDel(True)
 
-# if __name__ == '__main__':
-#     app.run_server(debug=True)
+if __name__ == '__main__':
+    app.run_server(debug=True)
